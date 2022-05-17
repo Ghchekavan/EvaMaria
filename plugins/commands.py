5351a2a7a21471e86cc695afb82c4ca68eab257c
Ghchekavan <98634151+Ghchekavan@users.noreply.github.com>
2022-05-17 20:17:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,10 +23,10 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
+                InlineKeyboardButton('🧑‍🎤ႮᏢᎠᎪͲᎬՏ🧑‍🎤', url='https://t.me/KL_MOVIES_MAIN_CHANNEL_01')
             ],
             [
-                InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
+                InlineKeyboardButton('🐬 𝖍𝖊𝖑𝖕 🐬', url=f"https://t.me/{temp.U_NAME}?start=help"),
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -42,13 +42,13 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('❤️‍🔥 𝖆𝖉𝖉 𝖒𝖊 𝖙𝖔 𝖞𝖔𝖚𝖗 𝖌𝖗𝖔𝖚𝖕 ❤️‍🔥', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('🦋 𝖘𝖊𝖆𝖗𝖈𝖍 🦋', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('🦋 𝖈𝖍𝖆𝖓𝖓𝖊𝖑 🦋', url='https://t.me/KL_MOVIES_MAIN_CHANNEL_01')
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('🐬 𝖍𝖊𝖑𝖕 🐬', callback_data='help'),
+            InlineKeyboardButton('🐬 𝖆𝖇𝖔𝖚𝖙 🐬', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -88,13 +88,13 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('❤️‍🔥 𝖆𝖉𝖉 𝖒𝖊 𝖙𝖔 𝖞𝖔𝖚𝖗 𝖌𝖗𝖔𝖚𝖕 ❤️‍🔥', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('🦋 𝖘𝖊𝖆𝖗𝖈𝖍 🦋', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('🦋 𝖈𝖍𝖆𝖓𝖓𝖊𝖑 🦋', url='https://t.me/KL_MOVIES_MAIN_CHANNEL_01')
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('🐬 𝖍𝖊𝖑𝖕 🐬', callback_data='help'),
+            InlineKeyboardButton('🐬 𝖆𝖇𝖔𝖚𝖙 🐬', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
